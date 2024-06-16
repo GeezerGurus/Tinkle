0e0b4c6889b5e813dfc5d5dbe9e35b97d267cf29
Alastor428 <swanlinnhtun123@gmail.com>
2024-06-16 12:17:56 +0630

    Debt list main page

--- src/components/debt list/ClosedPage.jsx
+++ src/components/debt list/ClosedPage.jsx
@@ -1,73 +1,160 @@
 import React from "react";
-import { Box, Typography, useTheme } from "@mui/material";
+import {
+  Box,
+  Typography,
+  useTheme,
+  SpeedDial,
+  SpeedDialAction,
+} from "@mui/material";
 import { tokens } from "../../theme";
 import Debt from "./Debt";
+import PowerSettingsNewIcon from "@mui/icons-material/PowerSettingsNew";
+import AddIcon from "@mui/icons-material/Add";
+const actions = [
+  {
+    icon: (
+      <PowerSettingsNewIcon
+        sx={{ transform: "rotate(180deg)", color: "red" }}
+      />
+    ),
+    name: "Lend",
+  },
+  {
+    icon: <PowerSettingsNewIcon sx={{ color: "green" }} />,
+    name: "Save",
+  },
+];
 
 const ClosedPage = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   return (
     <Box
-      sx={{ display: "flex", flexDirection: "column", gap: theme.spacing(3) }}
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        gap: theme.spacing(3),
+        height: "100vh", // Set height to full viewport height
+        overflowY: "auto", // Enable vertical scrolling
+        padding: theme.spacing(2),
+        "::-webkit-scrollbar": {
+          width: "7px",
+        },
+        "::-webkit-scrollbar-track": {
+          background: "#EAE5E5",
+        },
+        "::-webkit-scrollbar-thumb": {
+          background: "#544D4D",
+          height: "86.56px",
+          borderRadius: "2px",
+        },
+        "::-webkit-scrollbar-thumb:hover": {
+          background: "#555",
+        },
+      }}
     >
-      {/* Lent part  */}
-      <Box>
-        {/* I lent  */}
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            backgroundColor: "yellow",
-          }}
-        >
+      {/* Common Scrollable Container */}
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: theme.spacing(3),
+        }}
+      >
+        {/* Lent part */}
+        <Box>
+          {/* I lent */}
           <Box
-            sx={{ width: "120px", height: "8px", backgroundColor: "#00F79E" }}
-          />
-          <Typography variant="title2">I Lent</Typography>
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              backgroundColor: "yellow",
+              padding: theme.spacing(2), // Add some padding
+            }}
+          >
+            <Box
+              sx={{ width: "120px", height: "8px", backgroundColor: "#00F79E" }}
+            />
+            <Typography variant="h6">I Lent</Typography>
+          </Box>
+          {/* Active Debt list */}
+          <Box
+            sx={{
+              width: "100%",
+              display: "flex",
+              flexDirection: "column",
+              gap: theme.spacing(1),
+            }}
+          >
+            <Debt bgColor="#FFF2F2" />
+            <Debt bgColor="#FFF2F2" />
+            <Debt bgColor="#FFF2F2" />
+            <Debt bgColor="#FFF2F2" />
+            <Debt bgColor="#FFF2F2" />
+          </Box>
         </Box>
-        {/*Active Debt list  */}
-        <Box
-          sx={{
-            width: "100%",
-            display: "flex",
-            flexDirection: "column",
-            gap: theme.spacing(1),
-          }}
-        >
-          <Debt bgColor="purple" />
-          <Debt bgColor="purple" />
+        {/* Owe Part */}
+        <Box>
+          {/* I Owe */}
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              backgroundColor: "yellow",
+              padding: theme.spacing(2), // Add some padding
+            }}
+          >
+            <Box
+              sx={{ width: "120px", height: "8px", backgroundColor: "#FF0000" }}
+            />
+            <Typography variant="h6">I Owe</Typography>
+          </Box>
+          {/* Closed Debt list */}
+          <Box
+            sx={{
+              width: "100%",
+              display: "flex",
+              flexDirection: "column",
+              gap: theme.spacing(1),
+            }}
+          >
+            <Debt bgColor="#FFF2F2" />
+            <Debt bgColor="#FFF2F2" />
+            <Debt bgColor="#FFF2F2" />
+            <Debt bgColor="#FFF2F2" />
+            <Debt bgColor="#FFF2F2" />
+            <Debt bgColor="#FFF2F2" />
+          </Box>
         </Box>
       </Box>
-      {/* Owe Part  */}
-      <Box>
-        {/* I Owe  */}
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            backgroundColor: "yellow",
-          }}
-        >
-          <Box
-            sx={{ width: "120px", height: "8px", backgroundColor: "#FF0000" }}
+      <SpeedDial
+        ariaLabel="SpeedDial example"
+        sx={{
+          position: "absolute",
+          bottom: -30,
+          right: 130,
+          width: "116px",
+          //backgroundColor: "#2099DD",
+        }}
+        icon={
+          <AddIcon
+            sx={{
+              //backgroundColor: "#2099DD",
+              color: "white",
+            }}
           />
-          <Typography variant="title2">I Owe</Typography>
-        </Box>
-        {/*Closed Debt ;ist  */}
-        <Box
-          sx={{
-            width: "100%",
-            display: "flex",
-            flexDirection: "column",
-            gap: theme.spacing(1),
-          }}
-        >
-          <Debt bgColor="purple" />
-          <Debt bgColor="purple" />
-        </Box>
-      </Box>
+        }
+      >
+        {actions.map((action) => (
+          <SpeedDialAction
+            key={action.name}
+            icon={action.icon}
+            tooltipTitle={action.name}
+          />
+        ))}
+      </SpeedDial>
     </Box>
   );
 };
--- src/components/debt list/Debt.jsx
+++ src/components/debt list/Debt.jsx
@@ -1,11 +1,147 @@
-import { Paper } from "@mui/material";
+import { Box, Button, Divider, Paper, Typography } from "@mui/material";
 import React from "react";
-
+const Due = ({ duedate }) => {
+  return (
+    <Box
+      sx={{
+        width: "1185px",
+        height: "29px",
+        display: "flex",
+        justifyContent: "space-between",
+        //backgroundColor: "red",
+      }}
+    >
+      <Button
+        sx={{
+          width: "161px",
+          height: "29px",
+          borderRadius: "4px",
+          backgroundColor: "black",
+          color: "white",
+        }}
+      >
+        +Add Record
+      </Button>
+      <Box
+        sx={{
+          width: "142px",
+          height: "29px",
+          fontSize: "16px",
+          lineHeight: "19.36px",
+          letterSpacing: "1%",
+          fontWeight: "500",
+          color: "#A3A3A3",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "end",
+        }}
+      >
+        {" "}
+        Due-
+        <Typography
+          sx={{
+            fontSize: "16px",
+            lineHeight: "19.36px",
+            letterSpacing: "1%",
+            fontWeight: "500",
+            color: "black",
+          }}
+        >
+          {duedate}
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
+const Cap = ({ name, reason, amount }) => {
+  return (
+    <Box
+      sx={{
+        width: "1185px",
+        height: "50px",
+        display: "flex",
+        justifyContent: "space-between",
+      }}
+    >
+      <Box
+        sx={{
+          height: "50px",
+          width: "592.5px",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <Typography
+          sx={{
+            fontSize: "34px",
+            lineHeight: "29.05px",
+            fontWeight: "600",
+            letterSpacing: "4%",
+            color: "black",
+          }}
+        >
+          {name}
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: "16px",
+            lineHeight: "19.36px",
+            fontWeight: "600",
+            letterSpacing: "4%",
+            color: "#A3A3A3",
+          }}
+        >
+          {reason}
+        </Typography>
+      </Box>
+      <Box
+        sx={{
+          height: "100%",
+          width: "592.5px",
+          display: "flex",
+          justifyContent: "end",
+          alignItems: "center",
+          color: "black",
+        }}
+      >
+        <Typography
+          sx={{
+            fontSize: "19px",
+            lineHeight: "22.99px",
+            fontWeight: "500",
+            letterSpacing: "1%",
+          }}
+        >
+          {amount}
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
 const ActiveDebt = ({ bgColor }) => {
   return (
     <Paper
-      sx={{ width: "1240px", height: "175px", backgroundColor: bgColor }}
-    ></Paper>
+      sx={{
+        width: "1240px",
+        height: "175px",
+        backgroundColor: bgColor,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "space-evenly",
+      }}
+    >
+      <Cap name={"Daniel"} reason={"For his car"} amount={"500,000.00 MMK"} />
+      <Divider
+        sx={{
+          backgroundColor: "#00000033",
+          width: "calc(100% - 54px)",
+          height: "2px",
+          //backgroundColor: "black",
+        }}
+      />
+      <Due duedate={"02-01-2024"} />
+    </Paper>
   );
 };
 

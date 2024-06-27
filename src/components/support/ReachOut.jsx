a932c1404511f37f5f8eabc2834157dd8c14c076
Luck173 <yeyintnaingoo173@gmail.com>
2024-06-28 01:33:32 +0630

    support_page

---
+++ src/components/support/ReachOut.jsx
@@ -0,0 +1,49 @@
+import React from "react"
+import LocalPhoneRoundedIcon from '@mui/icons-material/LocalPhoneRounded';
+import { 
+    useTheme, 
+    Box,
+    Paper,
+    Typography, 
+    
+    } from "@mui/material";
+import { tokens } from "../../theme";
+ 
+const ReachOut=({Title, Description})=>{
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+
+    return(
+        <Paper sx={{
+            width:"390px",
+            height:"370px",
+            padding:"56px",
+            borderRadius:"18px",
+            textAlign:"Center",
+            backgroundColor:colors.extra["faint_white"]
+            
+        }} elevation={4} >
+                <Box 
+                sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    marginBottom: "33px",
+                }}
+                >
+                    <LocalPhoneRoundedIcon sx={{ fontSize: "72px", color:"green" }} />
+                </Box>
+                <Typography gutterBottom variant="h5" sx={{marginBottom:"33px"}} >
+                    Reach Out Directly
+                </Typography>
+                <Typography variant="body1" >
+                    Prefer to send us and <br/>
+                    email or give us a call?<br />
+                    Email: <a href="" style={{ textDecoration: "none"}} >geezersco@gmail.com</a>
+                </Typography>
+                
+        </Paper>
+       
+    )
+}
+export default ReachOut;
---
+++ src/components/support/index.js
@@ -0,0 +1,4 @@
+export { default as Experience } from "./Experience";
+export { default as ReachOut } from "./ReachOut";
+export { default as Assistance} from "./Assistance"
+export { default as SupportAccordion} from "./SupportAccordion"

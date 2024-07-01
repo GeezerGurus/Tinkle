27e08118057bbea93400500fac9b29bc725506f6
Thuta Htun <thutahtun608@gmail.com>
2024-07-01 12:04:53 +0630

    Responsive Sidebar and Dashboard Completed

--- src/components/statistics/Expenses.jsx
+++ src/components/statistics/Expenses.jsx
@@ -1,7 +1,7 @@
 import { Box, Paper, Typography, useTheme } from "@mui/material";
 import React from "react";
 import { tokens } from "../../theme";
-import PieActiveArc from "./PieChart";
+import PieActiveArc from "./Piechart";
 
 const pieData = [
   { id: 0, value: 88000, label: "Education and Development" },

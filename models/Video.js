068887c61ead732e458c562bfcc6c3aa633975ea
ZodNio <124388720+ZodNio@users.noreply.github.com>
2024-07-06 05:25:09 +0630

    Minor changes

--- models/Video.js
+++ models/Video.js
@@ -3,6 +3,11 @@ const Schema = mongoose.Schema;
 
 const videoSchema = new Schema(
   {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
     title: {
       type: String,
       required: true,

697ca2aa31d5a481bceee612fea8e4b7da21fae7
Wai Yan Htut <waiyanhtut354@gmail.com>
2024-07-06 18:44:01 +0630

    minor changes

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,72 +1,73 @@
 const User = require("../models/User");
 
 exports.addUser = async (req, res) => {
-    const { username, email, password, firstName } = req.body;
+  const { username, email, password, firstName } = req.body;
 
-    const user = new User({
-        username, 
-        email, 
-        password, 
-        firstName
-      });
+  const user = new User({
+    username,
+    email,
+    password,
+    firstName,
+  });
 
-      try {
-        if (!username || !email || !password || !firstName) {
-          return res.status(400).json({ message: "All fields are required!" });
-        }
-        if (password.length == 0 || password.length >= 32) {
-          return res.status(400).json({ message: "Password limit exceed or need password!" });
-        }
-        await user.save();
-        res.status(200).json({ message: "User Added" });
-      } catch (error) {
-        res.status(500).json({ message: error });
-      }
-      console.log(user);
+  try {
+    if (!username || !email || !password || !firstName) {
+      return res.status(400).json({ message: "All fields are required!" });
+    }
+    if (password.length == 0 || password.length >= 32) {
+      return res
+        .status(400)
+        .json({ message: "Password limit exceed or need password!" });
+    }
+    await user.save();
+    res.status(200).json({ message: "User Added" });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+  console.log(user);
 };
 
-exports.getUser = async (req, res) => {
+exports.getUsers = async (req, res) => {
   try {
-
-      const user = await User.find().sort({ createdAt: -1 });
-      if (!user) {
-          return res.status(404).json({ message: "User not found!" });
-      }
-      res.status(200).json(user);
+    const user = await User.find().sort({ createdAt: -1 });
+    if (!user) {
+      return res.status(404).json({ message: "User not found!" });
+    }
+    res.status(200).json(user);
   } catch (error) {
-      res.status(500).json({ message: error });
+    res.status(500).json({ message: error });
   }
 };
 
-exports.patchUser = async (req,res) => {
+exports.patchUser = async (req, res) => {
   const { userId } = req.params;
 
   try {
-        const user = await User.findOne({ _id: userId });
-        if (!user) {
-            return res.status(404).json({ message: "User not found!" });
-        }
-
-        if (req.body.newName){
-          user.username = req.body.newName;
-        }
-
-        if (req.body.email) {
-            user.email = req.body.email;
-        }
-        if (req.body.firstName) {
-            user.firstName = req.body.firstName;
-        }
-        if (req.body.lastName) {
-            user.lastName = req.body.lastName;
-        }
-
-        await user.save();
-
-        res.status(200).json(user);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
+    const user = await User.findOne({ _id: userId });
+    if (!user) {
+      return res.status(404).json({ message: "User not found!" });
     }
+
+    if (req.body.newName) {
+      user.username = req.body.newName;
+    }
+
+    if (req.body.email) {
+      user.email = req.body.email;
+    }
+    if (req.body.firstName) {
+      user.firstName = req.body.firstName;
+    }
+    if (req.body.lastName) {
+      user.lastName = req.body.lastName;
+    }
+
+    await user.save();
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 exports.deleteUser = async (req, res) => {

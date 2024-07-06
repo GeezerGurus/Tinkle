50c434f9109a19e470c00474f89c5d3bfe49ee67
Wai Yan Htut <waiyanhtut354@gmail.com>
2024-07-06 14:47:10 +0630

    modified auth flow

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,24 +1,71 @@
-// src/context/AuthContext.js
-import React, { createContext, useState } from "react";
+import React, { useState, useEffect } from "react";
+import api from "../api/api"; // Ensure this is the correct path to your api module
+import Cookies from "js-cookie";
 
-export const AuthContext = createContext();
+const AuthContext = React.createContext();
 
-export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
+const AuthProvider = ({ children }) => {
+  const [auth, setAuth] = useState({
+    token: Cookies.get("token"),
+    user: null,
+  });
 
-  const login = (userData) => {
-    console.log("Inside Context: ", userData);
-    setUser(userData);
+  // useEffect(() => {
+  //   const checkAuth = async () => {
+  //     if (auth.token) {
+  //       try {
+  //         const response = await api.post("/login", {
+  //           headers: { Authorization: `Bearer ${auth.token}` },
+  //         });
+  //         setAuth((prev) => ({ ...prev, user: response.data }));
+  //       } catch (error) {
+  //         console.error("Failed to authenticate user", error);
+  //         logout();
+  //       }
+  //     }
+  //   };
+  //   checkAuth();
+  // }, [auth.token]);
+
+  const login = async (userData) => {
+    // Remove the destructuring for userData
+    try {
+      const response = await api.post("/login", userData);
+      const { token, user } = response.data; // Destructure user from response.data
+      Cookies.set("token", token, { expires: 1 }); // Set cookie with expiry (1 day in this case)
+      setAuth({ token, user });
+
+      setTimeout(logout, 24 * 60 * 60 * 1000); // 1 day in milliseconds
+    } catch (error) {
+      console.error("Login failed", error);
+    }
+  };
+
+  const signup = async (userData) => {
+    // Remove the destructuring for userData
+    try {
+      const response = await api.post("/signup", userData);
+      const { token, user } = response.data; // Destructure user from response.data
+      Cookies.set("token", token, { expires: 1 }); // Set cookie with expiry (1 day in this case)
+      setAuth({ token, user });
+
+      setTimeout(logout, 24 * 60 * 60 * 1000); // 1 day in milliseconds
+    } catch (error) {
+      console.error("Signup failed", error);
+    }
   };
 
   const logout = () => {
-    localStorage.removeItem("jwt");
-    setUser(null);
+    Cookies.remove("token");
+    setAuth({ token: null, user: null });
+    api.post("/logout"); // Uncomment and ensure the correct endpoint
   };
 
   return (
-    <AuthContext.Provider value={{ user, login, logout }}>
+    <AuthContext.Provider value={{ auth, login, signup, logout }}>
       {children}
     </AuthContext.Provider>
   );
 };
+
+export { AuthProvider, AuthContext };

9e5230e7157ad7158e0cd377ca60c0b8403509fd
ZodNio <124388720+ZodNio@users.noreply.github.com>
2024-07-07 01:22:33 +0630

    Put commented code for postman testing

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -4,7 +4,7 @@ const User = require("../models/User");
 const requireAuth = (req, res, next) => {
   const token =
     req.headers.authorization && req.headers.authorization.split(" ")[1];
-
+    //const token = req.cookies.jwt;
   if (token) {
     jwt.verify(token, "net ninja secret", (err, decodedToken) => {
       if (err) {
